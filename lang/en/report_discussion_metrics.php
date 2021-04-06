850e83b0684baeb7f7f92d3a4400da96e85f4af9
Takahiro NAKAHARA <nakahara@3strings.co.jp>
2021-04-06 09:51:48 +0900

    Change language.
    -> "Specific to this group"

--- lang/en/report_discussion_metrics.php
+++ lang/en/report_discussion_metrics.php
@@ -42,7 +42,7 @@ $string['multimedia'] = 'Multimedia';
 $string['reportstart'] = 'Start';
 $string['reportend'] = 'End';
 $string['reportfilter'] = 'Report filter';
-$string['onlygroupworks'] = 'Only as a chosen group';
+$string['onlygroupworks'] = 'Specific to this group';
 
 //Reminder mail
 $string['remindsubject'] = 'Reminder to participate in the international exchange';
